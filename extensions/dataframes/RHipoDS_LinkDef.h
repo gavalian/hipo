df4041c99bc799e4294ef8b26a0c8e28a334edc1
Maurik Holtrop <maurik@physics.unh.edu>
2022-08-08 20:13:49 -0400

    Turn off the linking of all other ROOT stuff

--- extensions/dataframes/RHipoDS_LinkDef.h
+++ extensions/dataframes/RHipoDS_LinkDef.h
@@ -3,6 +3,11 @@
 //
 #ifdef __CLING__
 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link off all typedef;
+
 #pragma link C++ nestedtypedefs;
 #pragma link C++ nestedclasses;
 #pragma link C++ class RHipoDS;
